1a171443448714bf8a2640db3d86e42d7cc1f556
Muqaddam <muhammadmustufa555@gmail.com>
2023-03-12 11:11:20 +0500

    changes

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,27 +1,26 @@
-
 </section>
-        </div>
-    </div>
-    <!-- <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/js/custom.js')}}"></script>
-<script src="{{asset('assets/bundles/sweetalert/sweetalert.min.js')}}"></script>
+</div>
+</div>
+<!-- <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
+<script src="{{ asset('assets/bundles/sweetalert/sweetalert.min.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script> -->
 
-<script src="{{asset('assets/bundles/datatables/datatables.min.js')}}"></script>
-<script src="{{asset('assets/bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js')}}"></script>
-<script src="{{asset('assets/bundles/datatables/export-tables/dataTables.buttons.min.js')}}"></script>
-<script src="{{asset('assets/bundles/datatables/export-tables/buttons.flash.min.js')}}"></script>
-<script src="{{asset('assets/bundles/datatables/export-tables/jszip.min.js')}}"></script>
-<script src="{{asset('assets/bundles/datatables/export-tables/pdfmake.min.js')}}"></script>
-<script src="{{asset('assets/bundles/datatables/export-tables/vfs_fonts.js')}}"></script>
-<script src="{{asset('assets/bundles/datatables/export-tables/buttons.print.min.js')}}"></script>
-<script src="{{asset('assets/js/page/datatables.js')}}"></script>
+<script src="{{ asset('assets/bundles/datatables/datatables.min.js') }}"></script>
+<script src="{{ asset('assets/bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/bundles/datatables/export-tables/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('assets/bundles/datatables/export-tables/buttons.flash.min.js') }}"></script>
+<script src="{{ asset('assets/bundles/datatables/export-tables/jszip.min.js') }}"></script>
+<script src="{{ asset('assets/bundles/datatables/export-tables/pdfmake.min.js') }}"></script>
+<script src="{{ asset('assets/bundles/datatables/export-tables/vfs_fonts.js') }}"></script>
+<script src="{{ asset('assets/bundles/datatables/export-tables/buttons.print.min.js') }}"></script>
+<script src="{{ asset('assets/js/page/datatables.js') }}"></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/js/custom.js')}}"></script>
-<script src="{{asset('assets/bundles/sweetalert/sweetalert.min.js')}}"></script>
+<script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
+<script src="{{ asset('assets/bundles/sweetalert/sweetalert.min.js') }}"></script>
 <!-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> -->
 <script src="https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js"></script>
